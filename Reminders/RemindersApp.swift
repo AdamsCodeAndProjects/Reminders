b5124609b851bbefc611b7ee95a52c60fdf8a014
Adam Januszewski <Adpjan@att.net>
2022-06-10 11:00:58 -0400

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by adam janusewski on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
